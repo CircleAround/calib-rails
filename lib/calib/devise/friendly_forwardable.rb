20b8bd6e78ac910301a934b617cbf7c5096a231b
ms2sato <ms2.sato@gmail.com>
2018-03-08 08:17:53 +0900

    fix fail without root_path

--- lib/calib/devise/friendly_forwardable.rb
+++ lib/calib/devise/friendly_forwardable.rb
@@ -3,7 +3,7 @@ module Calib::Devise::FriendlyForwardable
 
   included do
     # @see https://github.com/plataformatec/devise/wiki/How-To:-Redirect-back-to-current-page-after-sign-in,-sign-out,-sign-up,-update
-    before_action :store_access_location!, if: :storable_location?
+    before_action :store_location!, if: :storable_location?
   end
 
   private
@@ -15,18 +15,18 @@ module Calib::Devise::FriendlyForwardable
   # path with stored_location or root
   # @see https://github.com/plataformatec/devise/wiki/How-To:-redirect-to-a-specific-page-on-successful-sign-in
   def friendly_forwarding_path(r = devise_key_for_store_location)
-    request.env['omniauth.origin'] || stored_location_for(r) || root_path
+    request.env['omniauth.origin'] || stored_location_for(r) || signed_in_root_path(r)
   end
 
   def storable_location?
     request.get? &&
       is_navigational_format? &&
       !devise_controller? &&
-      request.fullpath != root_path && # for return on creating new account
+      request.fullpath != signed_in_root_path(devise_key_for_store_location) && # for return on creating new account
       !request.xhr?
   end
 
-  def store_access_location!
+  def store_location!
     Rails.logger.debug("store_#{devise_key_for_store_location}_location!: #{request.fullpath}")
     store_location_for(devise_key_for_store_location, request.fullpath)
   end
