50014724907edff855a44b6d37b01d594a21abea
ms2sato <ms2.sato@gmail.com>
2018-03-09 09:29:54 +0900

    rdocs for basic_auth

--- lib/calib/controllers/basic_auth.rb
+++ lib/calib/controllers/basic_auth.rb
@@ -1,40 +1,45 @@
+# == A Module for Basic Authentication 
+# authenticated status stored in session.
+# avoid frequently logging in
+#
 # [usage]
-# include Calib::Controller::BasicAuth
+# class ApplicationController < ActionController::Base
+#   include Calib::Controllers::BasicAuth
+#   basic_auth(user: 'myuser', pass: 'secret')
+#  ...
+# end
+# 
 # basic_auth # read ENV['BASIC_AUTH_USER'] and ENV['BASIC_AUTH_PASS']
 # basic_auth(user: 'myuser', pass: 'secret') # standard
 #
 # basic_auth(user: 'myuser', pass: 'secret') do
 #   request.domain != 'localhost' # localhost is unblocked domain.
 # end
-module Calib
-  module Controllers
-    module BasicAuth
-      extend ActiveSupport::Concern
+module Calib::Controllers::BasicAuth
+  extend ActiveSupport::Concern
 
-      included do
-        class_attribute :basic_auth_user, :basic_auth_pass, :basic_auth_session_key, :basic_auth_block
+  included do
+    class_attribute :basic_auth_user, :basic_auth_pass, :basic_auth_session_key, :basic_auth_block
 
-        def self.basic_auth(user: ENV['BASIC_AUTH_USER'], password: ENV['BASIC_AUTH_PASS'], key: :basic_auth, &block)
-          self.basic_auth_user = user
-          self.basic_auth_pass = password
-          self.basic_auth_session_key = key
-          self.basic_auth_block = block
-        end
-
-        before_action -> {
-                        return if self.class.basic_auth_user.blank?
-                        return if session[self.class.basic_auth_session_key].present?
-                        return if self.class.basic_auth_block.present? && !self.class.basic_auth_block.call(request)
-
-                        authenticate_or_request_with_http_basic do |user, pass|
-                          if user == self.class.basic_auth_user && pass == self.class.basic_auth_pass
-                            session[self.class.basic_auth_session_key] = true
-                            return true
-                          end
-                          false
-                        end
-                      }
-      end
+    def self.basic_auth(user: ENV['BASIC_AUTH_USER'], password: ENV['BASIC_AUTH_PASS'], key: :basic_auth, &block)
+      self.basic_auth_user = user
+      self.basic_auth_pass = password
+      self.basic_auth_session_key = key
+      self.basic_auth_block = block
     end
+
+    before_action -> {
+                    return if self.class.basic_auth_user.blank?
+                    return if session[self.class.basic_auth_session_key].present?
+                    return if self.class.basic_auth_block.present? && !self.class.basic_auth_block.call(request)
+
+                    authenticate_or_request_with_http_basic do |user, pass|
+                      if user == self.class.basic_auth_user && pass == self.class.basic_auth_pass
+                        session[self.class.basic_auth_session_key] = true
+                        return true
+                      end
+                      false
+                    end
+                  }
   end
 end
