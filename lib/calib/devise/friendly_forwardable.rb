c240cb880688f372720c1a9c3b4f4aaa7a5aa6b4
ms2sato <ms2.sato@gmail.com>
2018-03-09 09:01:40 +0900

    rdoc for FriendlyForwardable

--- lib/calib/devise/friendly_forwardable.rb
+++ lib/calib/devise/friendly_forwardable.rb
@@ -1,23 +1,35 @@
+# == A Module for Friendly forwarding
+# Redirect back to current page after sign in.
+# @see https://github.com/plataformatec/devise/wiki/How-To:-Redirect-back-to-current-page-after-sign-in,-sign-out,-sign-up,-update
+# 
+# [usage]
+# class ApplicationController < ActionController::Base
+#   include Calib::Devise::FriendlyForwardable
+#  ...
+# end
+
 module Calib::Devise::FriendlyForwardable
   extend ActiveSupport::Concern
 
   included do
-    # @see https://github.com/plataformatec/devise/wiki/How-To:-Redirect-back-to-current-page-after-sign-in,-sign-out,-sign-up,-update
     before_action :store_location!, if: :storable_location?
   end
 
   private
 
+  # overwrite _after_sign_in_path_for_ provided Devise
   def after_sign_in_path_for(resource)
     friendly_forwarding_path(resource)
   end
 
-  # path with stored_location or root
+  # path with stored_location or root for redirect
+  # 
   # @see https://github.com/plataformatec/devise/wiki/How-To:-redirect-to-a-specific-page-on-successful-sign-in
   def friendly_forwarding_path(r = devise_key_for_store_location)
     request.env['omniauth.origin'] || stored_location_for(r) || signed_in_root_path(r)
   end
 
+  # if this is true, store location.
   def storable_location?
     request.get? &&
       is_navigational_format? &&
@@ -26,11 +38,13 @@ module Calib::Devise::FriendlyForwardable
       !request.xhr?
   end
 
+  # sotore current page's location 
   def store_location!
     Rails.logger.debug("store_#{devise_key_for_store_location}_location!: #{request.fullpath}")
     store_location_for(devise_key_for_store_location, request.fullpath)
   end
 
+  # primary devise key for storing location
   def devise_key_for_store_location
     Devise.mappings.keys[0]
   end
